45fefbc8c9943c93e854e782cb5677df7caf9d8b
marc-eli <https://github.com/Kemrone/TP_Prog_Mobile>
2021-04-20 19:47:27 +0200

    first commit

---
+++ TP6/app/src/main/java/com/example/myapplication/Planete.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Planete {
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(name = "name")
+    private String nom;
+
+    @ColumnInfo(name = "size")
+    private int taille;
+
+    Planete(int uid, String nom, int taille){
+        this.uid = uid;
+        this.nom = nom;
+        this.taille = taille;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+
+}
